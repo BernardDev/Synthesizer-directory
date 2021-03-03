383a17aa25fb521b804914a3a2c071f24c010f53
BernardDev <bernard.devworks@gmail.com>
2021-03-03 16:31:03 +0100

    Install Sass display synth images

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,3 +1,5 @@
+import './Fetch.scss';
+
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
@@ -5,24 +7,34 @@ const Fetch = () => {
   const BASEURL =
     'https://synthesizer-api.herokuapp.com/api/synths?key=C6T2MA6-8B54BSG-GZKKV0F-MFKC1S1';
   const INITIALSTATE = {status: 'idle', code: null, message: null};
-  const [synths, setSynths] = useState();
+  const [synths, setSynths] = useState([]);
   const [data, setData] = useState(INITIALSTATE);
 
-  useEffect(async () => {
-    const result = await axios.get(`${BASEURL}`);
-    setData({
-      ...data,
-      status: 'done',
-      code: result.status,
-      message: result.statusText,
-    });
-    setSynths(result.data.synths);
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get(`${BASEURL}`);
+      setData({
+        ...data,
+        status: 'done',
+        code: result.status,
+        message: result.statusText,
+      });
+      setSynths(result.data.synths);
+    }
+    fetchData();
   }, []);
 
   console.log('synths', synths);
-  console.log('data', data);
+  // console.log('data', data);
 
-  return <></>;
+  return (
+    <div>
+      {synths.map((synth, index) => {
+        return <div key={index}> {<img key={index} src={synth.img} />}</div>;
+      })}
+      <br />
+    </div>
+  );
 };
 
 export default Fetch;
