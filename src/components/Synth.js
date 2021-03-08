4cedf74db146af007c2e0121ef106a37c92891fe
BernardDev <bernard.devworks@gmail.com>
2021-03-08 18:07:32 +0100

    Refactor small cleanup

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -105,41 +105,19 @@ to {
 }`;
 // ---------
 
-// import styled, { css } from 'styled-components'
-
-// const complexMixin = css`;
-//   color: ${props => (props.whiteColor ? 'white' : 'black')};
-// `
-
-// const StyledComp = styled.div`
-//   /* This is an example of a nested interpolation */
-//   ${props => (props.complex ? complexMixin : 'color: blue;')};
-// `
-
 const SynthItem = styled.div`
   background-color: pink;
   flex-grow: 0;
-  /* transition: 2s; */
-  /* animation: ${cdBounce2} 0.6s; */
-  /* visibility: ${(props) => (props.show ? 'visible' : 'hidden')}; */
 `;
 
 const bounceAnimationLeft = css`
-  /* transform: translateX(0px); */
   animation: ${appearLeft} 0.7s ease;
   animation-fill-mode: forwards;
-  /* animation-duration: 2s; */
-  /* transition: 2s; */
-  /* transition-delay: 2s; */
 `;
 
 const bounceAnimationRight = css`
-  /* transform: translateX(-200px); */
-  /* animation-duration: 2s; */
   animation: ${appearRight} 0.7s ease;
   animation-fill-mode: forwards;
-  transition: 2s;
-  /* transition-delay: 2s; */
 `;
 
 const SynthContainer = styled.div`
@@ -149,9 +127,6 @@ const SynthContainer = styled.div`
   position: relative;
   width: 700px;
   margin-bottom: 10px;
-  /* background-color: rgba(255, 255, 0, 0.2); */
-  /* visibility: ${(props) => (props.show ? 'none' : 'hidden')}; */
-  /* background-color: ${(props) => (props.show ? 'red' : 'green')}; */
   &:nth-child(even) {
     flex-direction: row-reverse;
     ${(props) => {
@@ -165,35 +140,6 @@ const SynthContainer = styled.div`
     }}
   }
 `;
-// ---------
-
-// const cdBounce2mixin = css`
-// animation-name: cdBounce2;
-// animation-duration: 0.6s;
-// `;
-
-// ----
-// const SynthItem = styled.div`
-//   background-color: pink;
-//   flex-grow: 0;
-// `;
-
-// const SynthContainer = styled.div`
-//   display: flex;
-//   justify-content: flex-end;
-//   position: relative;
-//   width: 700px;
-//   margin-bottom: 10px;
-//   background-color: rgba(255, 255, 0, 0.2);
-//   transition: 2s;
-//   background-color: ${(props) => (props.show ? 'red' : 'green')};
-//   /* animation: ${(props) => (props.show ? cdBounce2 : '')}; */
-//   &:nth-child(even) {
-//     flex-direction: row-reverse;
-//     /* animation-name: ${cdBounce2inverse}; */
-//   }
-// `;
-// ----
 
 const Img = styled.img`
   height: auto;
